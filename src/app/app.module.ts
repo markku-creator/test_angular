1a798bf16fdcea67893950fb888a66e706951dac
markku-creator <markku.nissinen@ictnissinen.fi>
2020-04-20 00:26:39 +0300

    testing material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconRegistry, MatIconModule, } from '@angular/material/icon';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, HttpModule, MatButtonModule, MatIconModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
